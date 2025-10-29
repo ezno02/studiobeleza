e8faa81158682671197426354d0b905aa4099bb0
ezno02 <enzo.bmachado.e@gmail.com>
2025-10-29 10:23:36 -0300

    Correcao footer in appJsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import './App.css'
 import Header from './components/Header'
-import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [scrollAtingido, setScrollAtingido] = useState(false);
